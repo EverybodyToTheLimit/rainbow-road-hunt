8a4c1c50478a443d5085be218cee38b8a0ac56f6
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2023-03-16 16:50:49 +0000

    add confetti O_O

--- src/components/Takeover.js
+++ src/components/Takeover.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { leaderboard } from './DataLoader'
+import Confetti from 'react-confetti'
 
 
 export const Takeover = ({callback, time}) => {
@@ -13,9 +14,9 @@ export const Takeover = ({callback, time}) => {
     let something = result.sort((val1, val2) => (val1.time > val2.time) ? 1 : (val1.time < val2.time) ? -1 : 0)
     let result1 = []
     something.forEach((el, i) => {
-        result1.push(<li className="result-position">{i}</li>)
-        result1.push(<li className="result-initials">{el.initials}</li>)
-        result1.push(<li className="result-time">{el.time}</li>)
+        result1.push(<li key={i+"l1"} className="result-position">{i+1}</li>)
+        result1.push(<li key={i+"l2"} className="result-initials">{el.initials}</li>)
+        result1.push(<li key={i+"l3"} className="result-time">{el.time}</li>)
     })
     setLeaderTable(result1)
 },[])
@@ -24,15 +25,17 @@ export const Takeover = ({callback, time}) => {
 
   return (
     <div id="takeover">
-        <h1>You solved the puzzle!</h1>
-        <h2>Your time was: 
-        {(time.minutes !== 0) ? time.minutes + "m ": null} {(time.seconds !== 0) ? time.seconds + "s" : null}</h2>
-        <h4>Here's how your score compares to others</h4>
-          <ul id="scoreboard">
-            <li className="result-header">Position</li><li >Initials</li><li>Time</li>
-            {leaderTable}
-          </ul>
-        <button onClick={callback}>Start New Game?</button>
+          <h1>You solved the puzzle!</h1>
+          <h2>Your time was:
+          {(time.minutes !== 0) ? time.minutes + "m ": null} {(time.seconds !== 0) ? time.seconds + "s" : null}</h2>
+          <h4>Here's how your score compares to others</h4>
+            <ul id="scoreboard">
+              <li className="result-header">Position</li><li >Initials</li><li>Time</li>
+              {leaderTable}
+            </ul>
+          <button onClick={callback}>Start New Game?</button>
+          <Confetti/>
+
     </div>
   )
 }
