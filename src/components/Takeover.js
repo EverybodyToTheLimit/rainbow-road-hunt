4784bc30b5f2a9e5c530b40714bd2e21da71a470
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2023-03-21 15:46:00 +0000

    plug in firestore to hold and update high scores

--- src/components/Takeover.js
+++ src/components/Takeover.js
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import Confetti from 'react-confetti'
+import { getFirestore, getDoc, setDoc, doc } from 'firebase/firestore/lite'
+import { app } from './firebaseConfig'
 
 export const Takeover = ({callback, time}) => {
 
+  let [leaderboard, setLeaderboard] = useState([])
   let [leaderTable, setLeaderTable] = useState(null)
   let [playerName, setPlayername] = useState("")
   let [updatedLeaderboard, setUpdatedLeaderboard] = useState(null)
   let [submitted, setSubmitted] = useState(false)
 
-
-  let leaderboard = [
-    {"initials" : "BAR", "time": 6},
-    {"initials" : "BAZ", "time": 18},
-    {"initials" : "BOO", "time": 123},
-  ]
+  const db = getFirestore(app);
 
   let updateLeaderboard = (event) => {
       event.preventDefault();
@@ -29,25 +27,64 @@ export const Takeover = ({callback, time}) => {
     })
 
       setLeaderTable(result1)
+
+      //write to firebase
+
+      let updateStorage = async () => {
+
+
+        // Add a new message entry to the Firebase database.
+        try {
+            let docRef = doc(db, 'scoreboard', 'ltpQZkmzCTIx1Jj1SLRV')
+            let withoutActive = updatedTable.map(({active, ...items}) => items)
+            let jsonized = JSON.stringify(withoutActive)
+            let data = {
+                "currentScoreboard" : jsonized
+            }
+    
+            await setDoc(docRef, data)
+        }
+        catch(error) {
+          console.error('Error writing new message to Firebase Database', error);
+        }
+      }
+
+      updateStorage()
+
       setSubmitted(true)
+
   }
 
   let updatePlayer = (event) => {
     setPlayername(event.target.value)
   }
 
+
+
+
+
   useEffect(() => {
-    setSubmitted(false)
-    let result = [...leaderboard, {"initials" : "YOU", "time" : (time.hours * 360 + time.minutes * 60 + time.seconds),"active" : true}]
-    let something = result.sort((val1, val2) => (val1.time > val2.time) ? 1 : (val1.time < val2.time) ? -1 : 0)
-    setUpdatedLeaderboard(something)
-    let result1 = []
-    something.forEach((el, i) => {
-        result1.push(<li key={i+"l1"} className="result-position">{i+1}</li>)
-        result1.push(<li key={i+"l2"} className="result-initials">{el.initials}</li>)
-        result1.push(<li key={i+"l3"} className="result-time">{new Date(el.time * 1000).toISOString().substring(14, 19)}</li>)
-    })
-    setLeaderTable(result1)
+    let getData = async () => {
+      const db = getFirestore(app);
+      let docRef = doc(db, 'scoreboard', 'ltpQZkmzCTIx1Jj1SLRV')
+      const retrievedDoc = await getDoc(docRef)
+      let retrieved = JSON.parse(retrievedDoc.data().currentScoreboard)
+      setLeaderboard(retrieved)
+      setSubmitted(false)
+
+      let result = [...retrieved, {"initials" : "YOU", "time" : (time.hours * 360 + time.minutes * 60 + time.seconds),"active" : true}]
+      let sorted = result.sort((val1, val2) => (val1.time > val2.time) ? 1 : (val1.time < val2.time) ? -1 : 0)
+      setUpdatedLeaderboard(sorted)
+      let result1 = []
+      sorted.forEach((el, i) => {
+          result1.push(<li key={i+"l1"} className="result-position">{i+1}</li>)
+          result1.push(<li key={i+"l2"} className="result-initials">{el.initials}</li>)
+          result1.push(<li key={i+"l3"} className="result-time">{new Date(el.time * 1000).toISOString().substring(14, 19)}</li>)
+      })
+      setLeaderTable(result1)
+  }
+
+  getData()
 },[])
 
 
@@ -64,7 +101,7 @@ export const Takeover = ({callback, time}) => {
             </ul>
           { !submitted ? <form onSubmit={updateLeaderboard}>
               <input type="text" value={playerName} onChange={updatePlayer}></input>
-              <button>submit</button>
+              <button>Submit your score</button>
             </form> : null}
            <button className="button-85" onClick={callback}>Start New Game?</button> 
           <Confetti/>
@@ -72,3 +109,4 @@ export const Takeover = ({callback, time}) => {
     </div>
   )
 }
+
